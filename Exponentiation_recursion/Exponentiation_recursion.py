2950d275370017292067784264d2ffa0d6f42d26
EvgeniiaTogochakova <124245850+EvgeniiaTogochakova@users.noreply.github.com>
2023-04-21 20:48:38 +0700

    Fixed Exponentiation_recursion.py

--- Exponentiation_recursion/Exponentiation_recursion.py
+++ Exponentiation_recursion/Exponentiation_recursion.py
@@ -4,13 +4,13 @@
 # A = 3; B = 5 -> 243 (3⁵)
 # A = 2; B = 3 ->
 
-def Exponentiation_rec(base, exponent):
+def exponentiation_rec(base, exponent):
     if exponent == 0:
         return 1
-    result = base * Exponentiation_rec(base, exponent - 1)
+    result = base * exponentiation_rec(base, exponent - 1)
     return result
 
 
 a = int(input('Введите основание степени: '))
 b = int(input('Введите показатель степени: '))
-print(f'{a} в степени {b} равно {Exponentiation_rec(a, b)}')
+print(f'{a} в степени {b} равно {exponentiation_rec(a, b)}')
